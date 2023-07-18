0cfda32ac0e1a8c5d83125055befa06a0b7a0beb
gift <gift.dolo2006@gmail.com>
2023-07-18 10:05:22 -0500

    Initial Commit

---
+++ Githubtest/GithubtestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubtestApp.swift
+//  Githubtest
+//
+//  Created by Gift on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct GithubtestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
